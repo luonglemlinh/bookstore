877f8c175c7e78079ed7fd499b146e0b84b3238c
luonglemlinh <levion287204@gmail.com>
2025-12-16 21:14:55 +0700

    :deploy

--- connect.php
+++ connect.php
@@ -1,10 +1,22 @@
 <?php
-$user="root";
-$pass="";
-$host="127.0.0.1";
-$db="qlbansach";
 
-$ocon=new mysqli($host,$user,$pass, $db);
+if (isset ($ENV[''])){
+    $host=$ENV['tramway.proxy.rlwy.net'];
+    $user=$ENV['root'];
+    $pass=$ENV['uktMiTvrWuwlgoGamKvRmDObGKJKtsKY'];
+    $db=$ENV['railway'];
+    $port = $ENV['3306'];
+    
+    $ocon=new mysqli($host,$user,$pass, $db);
+} else {
+    $user = "root";
+    $pass = "";
+    $host = "127.0.0.1";
+    $db = "qlbansach";
+
+    $ocon=new mysqli($host,$user,$pass, $db);
+}
+
 if ($ocon->connect_error){
     die("Kết nối lỗi ".$ocon->connect_error);
 }
